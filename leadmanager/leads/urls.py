09bc6ed44269e7d4f383d3fc3ac9155bcbcb5795
George253 <hertzsch2@live.de>
2020-09-06 23:55:09 +0200

    setting up basic django

---
+++ leadmanager/leads/urls.py
@@ -0,0 +1,8 @@
+from django.contrib.admindocs.urls import urlpatterns
+from rest_framework import routers
+from .api import LeadViewSet
+
+router = routers.DefaultRouter()
+router.register('api/leads', LeadViewSet, 'leads')
+
+urlpatterns = router.urls
