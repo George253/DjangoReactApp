fb91e9829bb30d68c2eedfef4f4a30536e6b98a6
George253 <hertzsch2@live.de>
2020-09-07 15:15:29 +0200

    set up react app final

---
+++ leadmanager/frontend/src/components/leads/Form.js
@@ -0,0 +1,11 @@
+import React, { PureComponent } from 'react'
+
+export default class Form extends PureComponent {
+    render() {
+        return (
+            <div>
+                <h1>Add Form</h1>
+            </div>
+        )
+    }
+}
---
+++ leadmanager/frontend/src/components/leads/Leads.js
@@ -0,0 +1,11 @@
+import React, { PureComponent } from 'react'
+
+export default class Leads extends PureComponent {
+    render() {
+        return (
+            <div>
+                <h1>Add Leads</h1>
+            </div>
+        )
+    }
+}
